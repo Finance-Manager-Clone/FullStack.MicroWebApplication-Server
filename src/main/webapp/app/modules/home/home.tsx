15cce5ae324fb97f1cb5c1be5c6762ee926d6c94
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2022-01-20 18:41:35 -0500

    More updates added

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,11 +1,13 @@
 import './home.scss';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Translate } from 'react-jhipster';
-import { Row, Col, Alert } from 'reactstrap';
-
-import { useAppSelector } from 'app/config/store';
+import { Translate, translate, ValidatedField } from 'react-jhipster';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Alert, Row, Col, Form } from 'reactstrap';
+import { useAppDispatch, useAppSelector } from 'app/config/store';
+import { useForm } from 'react-hook-form';
+import { Redirect, RouteComponentProps } from 'react-router-dom';
+import LoginModal from '../login/login-modal';
 
 export const Home = () => {
   const account = useAppSelector(state => state.authentication.account);
@@ -14,17 +16,19 @@ export const Home = () => {
     <div className = "backgroundArea">
     <Row>
       <Col md="3" className="pad">
-        <span className = "graph"></span>
+        <span className = "logo"></span>
       </Col>
       <Col className = "mainContent" md="9">
-        <h2>
-          <Translate contentKey="home.title">Welcome, Zip Bankers</Translate>
-        </h2>
-        <p className="lead">
-          <Translate contentKey="home.subtitle"></Translate>
-        </p>
         {account?.login ? (
           <div>
+            <h2>
+              <Translate contentKey="home.title">Welcome, Zip Bankers</Translate>
+            </h2>
+            
+            <p className="lead">
+              <Translate contentKey="home.subtitle"></Translate>
+            </p>
+
             <Alert color="success">
               <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
                 {account.login} is logged in.
@@ -32,40 +36,35 @@ export const Home = () => {
             </Alert>
           </div>
         ) : (
-          <div className = "signInArea">
-            <form action="action_page.php" method="post">
-              <div className="container">
-                  <label htmlFor="uname"><b>Username</b></label>
-                  <input type="text" placeholder="Enter Username" name="uname" required></input>
+        <div className = "signInArea">
+          
 
-                  <label htmlFor="psw"><b>Password</b></label>
-                  <input type="password" placeholder="Enter Password" name="psw" required></input>
+          <form action="action_page.php" method="post">
+            <div className="container">
+                <label htmlFor="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required></input>
 
-                  <button type="submit">Login</button>
-                </div>
-            </form>
+                <label htmlFor="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required></input>
 
-            <Alert>
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
+                <button type="submit">Login</button>
+              </div>
+          </form>
 
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
-                <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
-            </Alert>
+          <Alert>
+            <Link to="/login" className="alert-link">
+              <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
+            </Link>
+          </Alert>
 
-            <Alert>
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Create new account</Translate>
-              </Link>
-            </Alert>
-          </div>
-        )}
+          <Alert>
+            <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
+            <Link to="/account/register" className="alert-link">
+              <Translate contentKey="global.messages.info.register.link">Create new account</Translate>
+            </Link>
+          </Alert>
+        </div>
+      )}
       </Col>
     </Row>
     </div>
