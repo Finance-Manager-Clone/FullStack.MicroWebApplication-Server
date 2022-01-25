bbfdc2443f1b0ff9ea3f7fcadd6e3482e9840aca
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2022-01-24 21:48:33 -0500

    removed some useless code

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -12,66 +12,62 @@ import LoginModal from '../login/login-modal';
 export const Home = () => {
   const account = useAppSelector(state => state.authentication.account);
 
-    };
-
-    return (
-      <div className = "backgroundArea">
-      <Row>
-        <Col md="3" className="pad">
-          <span className = "logo"></span>
-        </Col>
-        <Col className = "mainContent" md="9">
-          {account?.login ? (
-            <div>
-              <h2>
-                <Translate contentKey="home.title">Welcome, Zip Bankers</Translate>
-              </h2>
-              
-              <p className="lead">
-                <Translate contentKey="home.subtitle"></Translate>
-              </p>
-
-              <Alert color="success">
-                <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
-                  {account.login} is logged in.
-                </Translate>
-              </Alert>
-            </div>
-          ) : (
-          <div className = "signInArea">
+  return (
+    <div className = "backgroundArea">
+    <Row>
+      <Col md="3" className="pad">
+        <span className = "logo"></span>
+      </Col>
+      <Col className = "mainContent" md="9">
+        {account?.login ? (
+          <div>
+            <h2>
+              <Translate contentKey="home.title">Welcome, Zip Bankers</Translate>
+            </h2>
             
+            <p className="lead">
+              <Translate contentKey="home.subtitle"></Translate>
+            </p>
 
-            <form action="action_page.php" method="post">
-              <div className="container">
-                  <label htmlFor="uname"><b>Username</b></label>
-                  <input type="text" placeholder="Enter Username" name="uname" required></input>
-
-                  <label htmlFor="psw"><b>Password</b></label>
-                  <input type="password" placeholder="Enter Password" name="psw" required></input>
-
-                  <button type="submit">Login</button>
-                </div>
-            </form>
-
-            <Alert>
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-            </Alert>
-
-            <Alert>
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Create new account</Translate>
-              </Link>
+            <Alert color="success">
+              <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
+                {account.login} is logged in.
+              </Translate>
             </Alert>
           </div>
-        )}
-        </Col>
-      </Row>
-      </div>
-    );
-  };
+        ) : (
+        <div className = "signInArea">
+
+          <form action="action_page.php" method="post">
+            <div className="container">
+                <label htmlFor="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required></input>
+
+                <label htmlFor="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required></input>
+
+                <button type="submit">Login</button>
+              </div>
+          </form>
+
+          <Alert>
+            <Link to="/login" className="alert-link">
+              <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
+            </Link>
+          </Alert>
+
+          <Alert>
+            <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
+            <Link to="/account/register" className="alert-link">
+              <Translate contentKey="global.messages.info.register.link">Create new account</Translate>
+            </Link>
+          </Alert>
+        </div>
+      )}
+      </Col>
+    </Row>
+    </div>
+  );
 };
 
 export default Home;
